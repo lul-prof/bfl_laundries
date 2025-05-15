7b1a3d9ffbc2b3f9be525af0a8a7386368794535
lul_prof <israelmutua539@gmail.com>
2025-05-15 10:57:36 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,18 +1,7 @@
 from app import create_app, db
-from app.models import User, Order, Service, Payment, OrderItem
 
 app = create_app()
 
-@app.shell_context_processor
-def make_shell_context():
-    return {
-        'db': db,
-        'User': User,
-        'Order': Order,
-        'Service': Service,
-        'Payment': Payment,
-        'OrderItem': OrderItem
-    }
     # Initialize database tables
     with app.app_context():
         db.create_all()
